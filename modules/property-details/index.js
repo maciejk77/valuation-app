e25924be4331ef3bc2b7ac48b7158dfe613a1075
Maciej Kurek <maciejk77@gmail.com>
2023-05-09 21:22:34 +0100

    Fetch data from /api/account endpoint

--- modules/property-details/index.js
+++ modules/property-details/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable max-statements */
+import { useState, useEffect } from 'react';
 import { add, format } from 'date-fns';
 import React from 'react';
 import { Button } from '../../components/button';
@@ -15,52 +16,37 @@ import {
   Flex,
 } from './style';
 
-const account = {
-  uid: '65156cdc-5cfd-4b34-b626-49c83569f35e',
-  deleted: false,
-  dateCreated: '2020-12-03T08:55:33.421Z',
-  currency: 'GBP',
-  name: '15 Temple Way',
-  bankName: 'Residential',
-  type: 'properties',
-  subType: 'residential',
-  originalPurchasePrice: 250000,
-  originalPurchasePriceDate: '2017-09-03',
-  recentValuation: { amount: 310000, status: 'good' },
-  associatedMortgages: [
-    {
-      name: 'HSBC Repayment Mortgage',
-      uid: 'fb463121-b51a-490d-9f19-d2ea76f05e25',
-      currentBalance: -175000,
-    },
-  ],
-  canBeManaged: false,
-  postcode: 'BS1 2AA',
-  lastUpdate: '2020-12-01T08:55:33.421Z',
-  updateAfterDays: 30,
-};
-
 const Detail = ({}) => {
+  const [{ account }, setData] = useState({});
+
+  useEffect(() => {
+    fetch('/api/account')
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   let mortgage;
-  const lastUpdate = new Date(account.lastUpdate);
-  if (account.associatedMortgages.length) {
+  const lastUpdate = new Date(account?.lastUpdate);
+  if (account?.associatedMortgages.length) {
     mortgage = account.associatedMortgages[0];
   }
 
   // move these methods to helper and import here
   const sincePurchase =
-    account.recentValuation.amount - account.originalPurchasePrice;
+    account?.recentValuation.amount - account?.originalPurchasePrice;
 
   const sincePurchasePercentage =
-    (sincePurchase / account.originalPurchasePrice) * 100;
+    (sincePurchase / account?.originalPurchasePrice) * 100;
 
   const numberOfYearsSincePurchase =
     // 'quick and dirty' method should refactor to more accurate based on Date object/s
-    account.lastUpdate.split('-')[0] -
-    account.originalPurchasePriceDate.split('-')[0];
+    account?.lastUpdate.split('-')[0] -
+    account?.originalPurchasePriceDate.split('-')[0];
 
   const annualAppreciation =
     sincePurchasePercentage / numberOfYearsSincePurchase;
+
+  if (!account) return <Inset>LOADING...</Inset>;
 
   return (
     <Inset>
